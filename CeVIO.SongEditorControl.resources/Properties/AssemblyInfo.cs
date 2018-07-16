1832589b9fb9fedc5545c8af9b8925b04e199d64
UlyssesWu <wdwxy12345@gmail.com>
2018-07-17 00:05:56 +0800

    [SongEditor] Init SongEditor trans project

---
+++ CeVIO.SongEditorControl.resources/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyCompany("VOICeVIO")]
+[assembly: AssemblyCopyright("© CeVIO & VOICeVIO")]
+[assembly: AssemblyDescription(null)]
+[assembly: AssemblyProduct("CeVIO.CN")]
+[assembly: AssemblyTitle("CeVIO.SongEditorControl")]
+[assembly: AssemblyTrademark("wdwxy12345@gmail.com")]
+[assembly: AssemblyVersion("6.1.11.1")]
+[assembly: NeutralResourcesLanguage("zh-CN")]
+[assembly: AssemblyCulture("zh-CN")]
