ff2e4ba6a06b5f1155d23e4135aa8f924fa56fb7
UlyssesWu <wdwxy12345@gmail.com>
2018-07-17 00:45:29 +0800

    [7Seg][Knob] Init gadgets

---
+++ CeVIO.KnobControl.resources/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyCompany("VOICeVIO")]
+[assembly: AssemblyCopyright("© CeVIO & VOICeVIO")]
+[assembly: AssemblyDescription(null)]
+[assembly: AssemblyProduct("CeVIO.CN")]
+[assembly: AssemblyTitle("CeVIO.KnobControl")]
+[assembly: AssemblyTrademark("wdwxy12345@gmail.com")]
+[assembly: AssemblyVersion("5.3.6.1")]
+[assembly: NeutralResourcesLanguage("zh-CN")]
+[assembly: AssemblyCulture("zh-CN")]
---
+++ CeVIO.SevenSegmentControl.resources/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("VOICeVIO")]
+[assembly: AssemblyCopyright("© CeVIO & VOICeVIO")]
+[assembly: AssemblyDescription(null)]
+[assembly: AssemblyProduct("CeVIO.CN")]
+[assembly: AssemblyTitle("SevenSegmentControl")]
+[assembly: AssemblyTrademark("wdwxy12345@gmail.com")]
+[assembly: AssemblyVersion("6.1.0.0")]
