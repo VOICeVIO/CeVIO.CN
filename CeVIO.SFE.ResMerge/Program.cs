9f011f43d8bb3fddaa1586546ce9d5e910ff0ced
Ulysses Wu <wdwxy12345@gmail.com>
2022-06-24 16:50:44 +0800

    [8.3.2.0] update translation

--- CeVIO.SFE.ResMerge/Program.cs
+++ CeVIO.SFE.ResMerge/Program.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Resources;
 using dnlib.DotNet;
@@ -19,6 +20,51 @@ namespace CeVIO.SFE.ResMerge
             if (args.Length < 2)
             {
                 Console.WriteLine("Usage: .exe <dll path> <resx path>");
+                return;
+            }
+            
+
+            if (args.Length == 3 && args[2] == "-b")
+            {
+                var inputFolder = args[0];
+                if (!Directory.Exists(inputFolder))
+                {
+                    Console.WriteLine($"{inputFolder} is not a folder.");
+                    return;
+                }
+                var outputFolder = args[1];
+                if (!Directory.Exists(outputFolder))
+                {
+                    Console.WriteLine($"{outputFolder} is not a folder.");
+                    return;
+                }
+
+                var dllList = new []
+                {
+                    "CeVIO AI", "CeVIO.KnobControl", "CeVIO.SevenSegmentControl", "CeVIO.SongEditorControl", "CeVIO.TalkAnalyzeControl",
+                    "CeVIO.ToolBarControl"
+                };
+
+                foreach (var dll in dllList)
+                {
+                    var resxPath = Path.Combine(outputFolder, dll, "Properties", "Resources.resx");
+                    var dllPath = Path.Combine(inputFolder, $"{dll}.resources.dll");
+
+                    Console.WriteLine($"Processing {dllPath} ...");
+
+                    try
+                    {
+                        var dic = ParseResDll(dllPath);
+                        AddResource(dic, resxPath, resxPath);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        //throw;
+                    }
+                }
+
+                Console.WriteLine("All done!");
                 return;
             }
 
